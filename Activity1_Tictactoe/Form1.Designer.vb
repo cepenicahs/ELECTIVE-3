5a0f49f3dd01404f74f2e174bfce0f7d9d443cae
cepenicah <cepenicah@gmail.com>
2022-05-28 17:24:12 +0800

    updates

--- Activity1_Tictactoe/Form1.Designer.vb
+++ Activity1_Tictactoe/Form1.Designer.vb
@@ -23,153 +23,250 @@ Partial Class frmTicTacToe
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
-        Me.Button4 = New System.Windows.Forms.Button()
-        Me.Button5 = New System.Windows.Forms.Button()
-        Me.Button6 = New System.Windows.Forms.Button()
-        Me.Button7 = New System.Windows.Forms.Button()
-        Me.Button8 = New System.Windows.Forms.Button()
-        Me.Button9 = New System.Windows.Forms.Button()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmTicTacToe))
+        Me.btn11 = New System.Windows.Forms.Button()
+        Me.btn12 = New System.Windows.Forms.Button()
+        Me.btn13 = New System.Windows.Forms.Button()
+        Me.btn21 = New System.Windows.Forms.Button()
+        Me.btn22 = New System.Windows.Forms.Button()
+        Me.btn23 = New System.Windows.Forms.Button()
+        Me.btn31 = New System.Windows.Forms.Button()
+        Me.btn32 = New System.Windows.Forms.Button()
+        Me.btn33 = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.btnPlay = New System.Windows.Forms.Button()
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
-        'Button1
+        'btn11
         '
-        Me.Button1.Font = New System.Drawing.Font("Segoe UI", 50.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Button1.Location = New System.Drawing.Point(57, 88)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(112, 99)
-        Me.Button1.TabIndex = 0
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btn11.BackColor = System.Drawing.Color.LavenderBlush
+        Me.btn11.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn11.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn11.Font = New System.Drawing.Font("Cooper Black", 50.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn11.ForeColor = System.Drawing.Color.Black
+        Me.btn11.Location = New System.Drawing.Point(92, 153)
+        Me.btn11.Name = "btn11"
+        Me.btn11.Size = New System.Drawing.Size(112, 99)
+        Me.btn11.TabIndex = 0
+        Me.btn11.UseVisualStyleBackColor = False
         '
-        'Button2
+        'btn12
         '
-        Me.Button2.Font = New System.Drawing.Font("Segoe UI", 50.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Button2.Location = New System.Drawing.Point(175, 88)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(112, 99)
-        Me.Button2.TabIndex = 1
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.btn12.BackColor = System.Drawing.Color.LavenderBlush
+        Me.btn12.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn12.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn12.Font = New System.Drawing.Font("Cooper Black", 50.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn12.ForeColor = System.Drawing.Color.Black
+        Me.btn12.Location = New System.Drawing.Point(210, 153)
+        Me.btn12.Name = "btn12"
+        Me.btn12.Size = New System.Drawing.Size(112, 99)
+        Me.btn12.TabIndex = 1
+        Me.btn12.UseVisualStyleBackColor = False
         '
-        'Button3
+        'btn13
         '
-        Me.Button3.Font = New System.Drawing.Font("Segoe UI", 50.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Button3.Location = New System.Drawing.Point(293, 88)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(112, 99)
-        Me.Button3.TabIndex = 2
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.btn13.BackColor = System.Drawing.Color.LavenderBlush
+        Me.btn13.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn13.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn13.Font = New System.Drawing.Font("Cooper Black", 50.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn13.ForeColor = System.Drawing.Color.Black
+        Me.btn13.Location = New System.Drawing.Point(328, 153)
+        Me.btn13.Name = "btn13"
+        Me.btn13.Size = New System.Drawing.Size(112, 99)
+        Me.btn13.TabIndex = 2
+        Me.btn13.UseVisualStyleBackColor = False
         '
-        'Button4
+        'btn21
         '
-        Me.Button4.Font = New System.Drawing.Font("Segoe UI", 50.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Button4.Location = New System.Drawing.Point(57, 193)
-        Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(112, 99)
-        Me.Button4.TabIndex = 3
-        Me.Button4.UseVisualStyleBackColor = True
+        Me.btn21.BackColor = System.Drawing.Color.LavenderBlush
+        Me.btn21.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn21.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn21.Font = New System.Drawing.Font("Cooper Black", 50.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn21.ForeColor = System.Drawing.Color.Black
+        Me.btn21.Location = New System.Drawing.Point(92, 258)
+        Me.btn21.Name = "btn21"
+        Me.btn21.Size = New System.Drawing.Size(112, 99)
+        Me.btn21.TabIndex = 3
+        Me.btn21.UseVisualStyleBackColor = False
         '
-        'Button5
+        'btn22
         '
-        Me.Button5.Font = New System.Drawing.Font("Segoe UI", 50.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Button5.Location = New System.Drawing.Point(175, 193)
-        Me.Button5.Name = "Button5"
-        Me.Button5.Size = New System.Drawing.Size(112, 99)
-        Me.Button5.TabIndex = 4
-        Me.Button5.UseVisualStyleBackColor = True
+        Me.btn22.BackColor = System.Drawing.Color.LavenderBlush
+        Me.btn22.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn22.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn22.Font = New System.Drawing.Font("Cooper Black", 50.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn22.ForeColor = System.Drawing.Color.Black
+        Me.btn22.Location = New System.Drawing.Point(210, 258)
+        Me.btn22.Name = "btn22"
+        Me.btn22.Size = New System.Drawing.Size(112, 99)
+        Me.btn22.TabIndex = 4
+        Me.btn22.UseVisualStyleBackColor = False
         '
-        'Button6
+        'btn23
         '
-        Me.Button6.Font = New System.Drawing.Font("Segoe UI", 50.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Button6.Location = New System.Drawing.Point(293, 193)
-        Me.Button6.Name = "Button6"
-        Me.Button6.Size = New System.Drawing.Size(112, 99)
-        Me.Button6.TabIndex = 5
-        Me.Button6.UseVisualStyleBackColor = True
+        Me.btn23.BackColor = System.Drawing.Color.LavenderBlush
+        Me.btn23.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn23.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn23.Font = New System.Drawing.Font("Cooper Black", 50.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn23.ForeColor = System.Drawing.Color.Black
+        Me.btn23.Location = New System.Drawing.Point(328, 258)
+        Me.btn23.Name = "btn23"
+        Me.btn23.Size = New System.Drawing.Size(112, 99)
+        Me.btn23.TabIndex = 5
+        Me.btn23.UseVisualStyleBackColor = False
         '
-        'Button7
+        'btn31
         '
-        Me.Button7.Font = New System.Drawing.Font("Segoe UI", 50.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Button7.Location = New System.Drawing.Point(57, 298)
-        Me.Button7.Name = "Button7"
-        Me.Button7.Size = New System.Drawing.Size(112, 99)
-        Me.Button7.TabIndex = 6
-        Me.Button7.UseVisualStyleBackColor = True
+        Me.btn31.BackColor = System.Drawing.Color.LavenderBlush
+        Me.btn31.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn31.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn31.Font = New System.Drawing.Font("Cooper Black", 50.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn31.ForeColor = System.Drawing.Color.Black
+        Me.btn31.Location = New System.Drawing.Point(92, 363)
+        Me.btn31.Name = "btn31"
+        Me.btn31.Size = New System.Drawing.Size(112, 99)
+        Me.btn31.TabIndex = 6
+        Me.btn31.UseVisualStyleBackColor = False
         '
-        'Button8
+        'btn32
         '
-        Me.Button8.Font = New System.Drawing.Font("Segoe UI", 50.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Button8.Location = New System.Drawing.Point(175, 298)
-        Me.Button8.Name = "Button8"
-        Me.Button8.Size = New System.Drawing.Size(112, 99)
-        Me.Button8.TabIndex = 7
-        Me.Button8.UseVisualStyleBackColor = True
+        Me.btn32.BackColor = System.Drawing.Color.LavenderBlush
+        Me.btn32.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn32.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn32.Font = New System.Drawing.Font("Cooper Black", 50.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn32.ForeColor = System.Drawing.Color.Black
+        Me.btn32.Location = New System.Drawing.Point(210, 363)
+        Me.btn32.Name = "btn32"
+        Me.btn32.Size = New System.Drawing.Size(112, 99)
+        Me.btn32.TabIndex = 7
+        Me.btn32.UseVisualStyleBackColor = False
         '
-        'Button9
+        'btn33
         '
-        Me.Button9.Font = New System.Drawing.Font("Segoe UI", 50.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Button9.Location = New System.Drawing.Point(293, 298)
-        Me.Button9.Name = "Button9"
-        Me.Button9.Size = New System.Drawing.Size(112, 99)
-        Me.Button9.TabIndex = 8
-        Me.Button9.UseVisualStyleBackColor = True
+        Me.btn33.BackColor = System.Drawing.Color.LavenderBlush
+        Me.btn33.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn33.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn33.Font = New System.Drawing.Font("Cooper Black", 50.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn33.ForeColor = System.Drawing.Color.Black
+        Me.btn33.Location = New System.Drawing.Point(328, 363)
+        Me.btn33.Name = "btn33"
+        Me.btn33.Size = New System.Drawing.Size(112, 99)
+        Me.btn33.TabIndex = 8
+        Me.btn33.UseVisualStyleBackColor = False
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 30.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Label1.Location = New System.Drawing.Point(124, 20)
+        Me.Label1.BackColor = System.Drawing.Color.Crimson
+        Me.Label1.Font = New System.Drawing.Font("Cooper Black", 39.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(82, 57)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(217, 54)
+        Me.Label1.Size = New System.Drawing.Size(358, 61)
         Me.Label1.TabIndex = 9
         Me.Label1.Text = "TICTACTOE"
         '
         'btnPlay
         '
-        Me.btnPlay.Location = New System.Drawing.Point(57, 412)
+        Me.btnPlay.BackColor = System.Drawing.Color.Crimson
+        Me.btnPlay.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnPlay.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnPlay.Font = New System.Drawing.Font("Cooper Black", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnPlay.ForeColor = System.Drawing.Color.White
+        Me.btnPlay.Location = New System.Drawing.Point(195, 500)
         Me.btnPlay.Name = "btnPlay"
-        Me.btnPlay.Size = New System.Drawing.Size(112, 40)
+        Me.btnPlay.Size = New System.Drawing.Size(140, 59)
         Me.btnPlay.TabIndex = 10
-        Me.btnPlay.Text = "Play"
-        Me.btnPlay.UseVisualStyleBackColor = True
+        Me.btnPlay.Text = "PLAY"
+        Me.btnPlay.UseVisualStyleBackColor = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(-13, -10)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(550, 681)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 11
+        Me.PictureBox1.TabStop = False
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.BackColor = System.Drawing.Color.Thistle
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(537, 24)
+        Me.MenuStrip1.TabIndex = 12
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
         '
         'frmTicTacToe
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(769, 488)
+        Me.ClientSize = New System.Drawing.Size(537, 651)
+        Me.ControlBox = False
         Me.Controls.Add(Me.btnPlay)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.Button9)
-        Me.Controls.Add(Me.Button8)
-        Me.Controls.Add(Me.Button7)
-        Me.Controls.Add(Me.Button6)
-        Me.Controls.Add(Me.Button5)
-        Me.Controls.Add(Me.Button4)
-        Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btn33)
+        Me.Controls.Add(Me.btn32)
+        Me.Controls.Add(Me.btn31)
+        Me.Controls.Add(Me.btn23)
+        Me.Controls.Add(Me.btn22)
+        Me.Controls.Add(Me.btn21)
+        Me.Controls.Add(Me.btn13)
+        Me.Controls.Add(Me.btn12)
+        Me.Controls.Add(Me.btn11)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "frmTicTacToe"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "TICTACTOE"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents Button1 As Button
-    Friend WithEvents Button2 As Button
-    Friend WithEvents Button3 As Button
-    Friend WithEvents Button4 As Button
-    Friend WithEvents Button5 As Button
-    Friend WithEvents Button6 As Button
-    Friend WithEvents Button7 As Button
-    Friend WithEvents Button8 As Button
-    Friend WithEvents Button9 As Button
+    Friend WithEvents btn11 As Button
+    Friend WithEvents btn12 As Button
+    Friend WithEvents btn13 As Button
+    Friend WithEvents btn21 As Button
+    Friend WithEvents btn22 As Button
+    Friend WithEvents btn23 As Button
+    Friend WithEvents btn31 As Button
+    Friend WithEvents btn32 As Button
+    Friend WithEvents btn33 As Button
     Friend WithEvents Label1 As Label
     Friend WithEvents btnPlay As Button
     Friend WithEvents Timer1 As Timer
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
 End Class
--- Activity1_Tictactoe/Form1.vb
+++ Activity1_Tictactoe/Form1.vb
@@ -1,29 +1,153 @@
 ﻿Public Class frmTicTacToe
     Dim flag = 0
-    Private randNum As Integer = 1
-    Private buttonID As Integer = 0
+    Dim turn = True
+    Dim count = 0
+
     Private Sub frmTicTacToe_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        Initial()
     End Sub
 
-    Private Sub Startgame()
-        Reset()
-
-        buttonID = 1
-        Timer1.Start()
+    Private Sub Initial()
+        btn11.Enabled = False
+        btn12.Enabled = False
+        btn13.Enabled = False
+        btn21.Enabled = False
+        btn22.Enabled = False
+        btn23.Enabled = False
+        btn31.Enabled = False
+        btn32.Enabled = False
+        btn33.Enabled = False
+        btn11.Text = ""
+        btn12.Text = ""
+        btn13.Text = ""
+        btn21.Text = ""
+        btn22.Text = ""
+        btn23.Text = ""
+        btn31.Text = ""
+        btn32.Text = ""
+        btn33.Text = ""
     End Sub
-
     Private Sub Reset()
+        btn11.BackColor = Color.LavenderBlush
+        btn12.BackColor = Color.LavenderBlush
+        btn13.BackColor = Color.LavenderBlush
+        btn21.BackColor = Color.LavenderBlush
+        btn22.BackColor = Color.LavenderBlush
+        btn23.BackColor = Color.LavenderBlush
+        btn31.BackColor = Color.LavenderBlush
+        btn32.BackColor = Color.LavenderBlush
+        btn33.BackColor = Color.LavenderBlush
+
+        btn11.Enabled = True
+        btn12.Enabled = True
+        btn13.Enabled = True
+        btn21.Enabled = True
+        btn22.Enabled = True
+        btn23.Enabled = True
+        btn31.Enabled = True
+        btn32.Enabled = True
+        btn33.Enabled = True
+        btn11.Text = ""
+        btn12.Text = ""
+        btn13.Text = ""
+        btn21.Text = ""
+        btn22.Text = ""
+        btn23.Text = ""
+        btn31.Text = ""
+        btn32.Text = ""
+        btn33.Text = ""
+
+        turn = True
+        count = 0
+
+    End Sub
+
+    Private Sub Checker()
+        'HORIZONTAL
+        Dim winner As Boolean
+        winner = False
+
+        If ((btn11.Text = btn12.Text) And (btn12.Text = btn13.Text) And (btn11.Enabled = False)) Then
+            winner = True
+        End If
+        If ((btn21.Text = btn22.Text) And (btn22.Text = btn23.Text) And (btn21.Enabled = False)) Then
+            winner = True
+        End If
+        If ((btn31.Text = btn32.Text) And (btn32.Text = btn33.Text) And (btn31.Enabled = False)) Then
+            winner = True
+        End If
+
+        'VERTICAL
+        If ((btn11.Text = btn21.Text) And (btn21.Text = btn31.Text) And (btn31.Enabled = False)) Then
+            winner = True
+        End If
+
+        If ((btn12.Text = btn22.Text) And (btn22.Text = btn32.Text) And (btn32.Enabled = False)) Then
+                winner = True
+            End If
+        If ((btn13.Text = btn23.Text) And (btn32.Text = btn33.Text) And (btn33.Enabled = False)) Then
+            winner = True
+        End If
+
+        'DIAGONAL
+        If ((btn11.Text = btn22.Text) And (btn22.Text = btn33.Text) And (btn33.Enabled = False)) Then
+            winner = True
+        End If
+        If ((btn13.Text = btn22.Text) And (btn22.Text = btn31.Text) And (btn31.Enabled = False)) Then
+            winner = True
+        End If
+
+        If (winner) Then
+            Dim player As String
+            player = ""
+
+            If (turn) Then
+                player = "O"
+            Else
+                player = "X"
+                MessageBox.Show("Congratulations Player " + player + "! You won the game!")
+            End If
+        Else
+            If (count = 9) Then
+                MessageBox.Show("It's a draw!", "Result")
+            End If
+        End If
     End Sub
 
     Private Sub btnPlay_Click(sender As Object, e As EventArgs) Handles btnPlay.Click
         If flag = 0 = True Then
             flag = 1
-            btnPlay.Text = "Reset"
+            btnPlay.Text = "RESET"
+            Reset()
         ElseIf flag = 1 = True Then
             flag = 0
-            btnPlay.Text = "Start"
+            btnPlay.Text = "PLAY"
+            Reset()
         End If
 
+    End Sub
+
+    Private Sub button_click(sender As Object, e As EventArgs) Handles btn33.Click, btn32.Click, btn31.Click, btn23.Click, btn22.Click, btn21.Click, btn13.Click, btn12.Click, btn11.Click
+        Dim b As Button = CType(sender, Button)
+        If (turn = True) Then
+            b.Text = "X"
+        Else
+            b.Text = "O"
+        End If
+
+        turn = Not (turn)
+        count += 1
+        b.BackColor = Color.Khaki
+        b.Enabled = False
+        Checker()
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Application.Exit()
     End Sub
 End Class
